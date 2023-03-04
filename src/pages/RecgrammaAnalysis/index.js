05ee57f18659805d9fa7c8adb608196159b2a9cf
郑欣怡 <2030964305@qq.com>
2023-03-04 20:21:38 +0800

    feat: 匹配字符串

--- src/pages/RecgrammaAnalysis/index.js
+++ src/pages/RecgrammaAnalysis/index.js
@@ -1,24 +1,150 @@
-import { Input, Button } from 'antd';
-import { useState } from 'react';
+import { Input, Button, List, Table } from 'antd';
+import { Item } from 'rc-menu';
+import { useEffect, useRef, useState } from 'react';
+import { Prod, LL1 } from './util.js';
 
 const { TextArea } = Input;
 
 export function Recgramma() {
-    const [G,setG ] = useState('');
-    const [prod, setProd] = useState('');
+    const [G, setG] = useState('');
+    const [str, setStr] = useState('');
+    const [firstfollow, setFirstFollow] = useState();
+    const [columns, setColumns] = useState([]);
+    const [select, setSelect] = useState([]);
+    const [stack, setStack] = useState([]);
+   
+
+    let LL = useRef(new LL1());
+    const stepColumns = [
+        {
+            title:'stack',
+            dataIndex:'gra',
+            key:'stack'
+        },
+        {
+            title:'input',
+            dataIndex: 'str',
+            key:'input',
+        },
+        {
+            title:'production',
+            dataIndex:'production',
+            key:'production'
+        }
+    ]
+    function startAnalysis() {
+        if (G === '') return;
+        let arr = G.split('\n');
+        let g = arr.map((item) => new Prod(item));
+        g.forEach((item) => {
+            LL.current.addProd(item);
+        });
+        arr.forEach((item) => {
+            LL.current.first(item);
+        })
+        LL.current.follow();
+        LL.current.parseTable();
+        console.log(LL.current.FIRST, LL.current.FOLLOW, LL.current.M, LL.current.VN, '0000');
+        let first = [];
+        let follow = [];
+        LL.current.FIRST.forEach((item, key) => {
+            first.push(`FIRST(${key}) = {${[...item]}}`);
+        })
+        LL.current.FOLLOW.forEach((value, key) => {
+            follow.push(`FOLLOW(${key}) = {${[...value]}}`);
+        });
+        setFirstFollow([...first, ...follow]);
+
+        const title = [{
+            title: 'key',
+            dataIndex: 'key',
+            key: 'key',
+        }, {
+            title: '#',
+            dataIndex: '#',
+            key: '#',
+        }];
+        LL.current.VT.forEach((value) => {
+            title.push({
+                title: value,
+                dataIndex: value,
+                key: value,
+            })
+        })
+        setColumns(title);
+        const data = [];
+        LL.current.VN.forEach((value) => {
+            data.push({
+                key: value
+            })
+        })
+        LL.current.M.forEach((value, key) => {
+            let keys = key.split(',');
+            data.map((item) => {
+                if (keys[0] === item.key) {
+                    item[keys[1]] = value;
+                }
+            })
+        })
+        setSelect(data)
+
+    }
+    function nextStep() {
+        //if(str.length === 0) return;
+        console.log(LL.current.VN, 78797)
+        let h = str.length === 1? LL.current.match(str) : LL.current.match(str.split(""));
+        if (h.length <= 1) {
+            setStr(h)
+        } else {
+            setStr(h.join(''));
+        }
+
+        const temp = {
+            gra: [...LL.current.gra],
+            str: h,
+            production: LL.current.production,
+        }
+        setStack([...stack, temp]);
+     //   console.log(LL.current.gra, h, 89898)
+    }
     return (
         <div>
             <div>可视递归下降分析</div>
             <div>
                 <div>
                     <p>输入文法</p>
-                    <TextArea rows={4} onChange={(e) => setG(e.target.value)}/>
+                    <TextArea rows={4} onChange={(e) => setG(e.target.value)} />
                     <p>输入产生式</p>
-                    <TextArea rows={4} onChange={(e) => setProd(e.target.value)}/>
-                    <Button>开始分析</Button>
+                    <TextArea rows={4} onChange={(e) => setStr(e.target.value)} />
+                    <Button onClick={startAnalysis}>开始分析</Button>
+                </div>
+                <div>
+                    <List
+                        header={<div>first&follow</div>}
+                        bordered
+                        dataSource={firstfollow}
+                        renderItem={(item) => <List.Item>{item}</List.Item>}
+                    />
+                </div>
+                <div>
+                    <p>预测分析表</p>
+                    <Table columns={columns} dataSource={select} />;
+                </div>
+                <div>
+                    递归分析步骤
+                    <div>
+                        <Button onClick={nextStep}>下一步</Button>
+                        <Table columns={stepColumns} dataSource={stack} />;
+                    </div>
                 </div>
             </div>
-            
+
         </div>
     )
 }
+
+// E->TG
+// G->+TG|@
+// T->FY
+// Y->*FY|@
+// F->(E)|i
--- src/pages/RecgrammaAnalysis/util.js
+++ src/pages/RecgrammaAnalysis/util.js
@@ -1,4 +1,4 @@
-class Prod {
+export class Prod {
     constructor(str) {
         this.prod = str;
         this.isValid = false;
@@ -16,15 +16,15 @@ class Prod {
         for (let i = 0; i < this.prod.length; ++i) {
             let c = this.prod[i];
             let isVn = regex.test(c)
-            //console.log(regex.test(c),c,'yy')
+            
             if (isVn) {
-                //  console.log(c,'ll')
+                
                 this.Vn.add(c);
             }
             else if (c !== '|' && !(c === '-' && this.prod[i + 1] === '>' && ++i))
                 this.Vt.add(c);
         }
-        console.log(this.prod, this.Vn, this.Vt)
+       
         for (let i = 3; i < this.prod.length; ++i) {
             let j;
             for (j = i + 1; j < this.prod.length && this.prod[j] !== '|'; ++j);
@@ -35,17 +35,21 @@ class Prod {
         return this.isValid = true;
 
     }
+
+ 
 }
 
 
-
-class LL1 {
+export class LL1 {
     constructor() {
         this.FIRST = new Map();
         this.FOLLOW = new Map();
         this.G = [];
-        this.VN = new Set();
-        this.VT = new Set();
+        this.VN = new Set(); //非终结符
+        this.VT = new Set(); //终结符
+        this.M = new Map();
+        this.gra = [];
+        this.production = '';
     }
     addProd(prod) {
         if (prod.isValid) {
@@ -66,6 +70,8 @@ class LL1 {
         else return false;
     }
 
+
+
     first(str) {
         let prod = new Prod(str);
         if (prod.isValid) {
@@ -85,7 +91,7 @@ class LL1 {
             return new Set(['@']);
         }
         else if (str.length === 1) {
-            //console.log(898, this.VT, this.VN)
+           
             if (this.VT.has(str)) {
                 return new Set([str])
             }
@@ -99,10 +105,9 @@ class LL1 {
                                 b = index;
                             }
                         })
-                        // console.log(b, 5657, str)
+                       
                         let f = this.first(this.G[b].prod);
-                        //console.log(f)
-
+                      
                         if (this.FIRST.has(str)) {
                             f.forEach((item) => this.FIRST.get(str).add(item))
                         } else {
@@ -115,13 +120,13 @@ class LL1 {
         } else {
             let ret = new Set();
             for (let i = 0; i < str.length; ++i) {
-                //console.log(str.substring(i, i + 1), 899)
+               
                 let f = this.first(str.substring(i, i + 1));
-                //console.log(f, 909);
+                
                 if (f.has('@') && str.length - 1 !== i) {
                     f.delete('@');
                     f.forEach((item) => ret.add(item))
-                    //  ret.add(...f)
+                    
                 } else {
                     f.forEach((item) => ret.add(item))
                     break;
@@ -141,28 +146,19 @@ class LL1 {
                 size = this.FOLLOW.get(pp.noTerminal).size;
                 for (let prod of this.G) {
                     let X = prod.noTerminal;
-                    //    console.log(prod,'X')
                     for (let p of this.G) {
                         for (let s of p.selection) {
-                            // console.log(typeof s);
                             let loc = s.split('').find((item) => item === X);
-                            //console.log(X, 'X')
+                            
                             if (loc) {
-                                console.log(s, s.indexOf(X),'KKK')
+                               
                                 let f;
                                 if (s.indexOf(X) + 1 >= s.length) {
                                     f = this.first('');
-                                    // f.forEach((item) => {
-                                    //     this.FOLLOW.get(X).add(item);
-                                    // })
+                                  
                                 } else {
                                      f = this.first(s[s.indexOf(X) + 1]);
-                                    // f.forEach((item) => {
-                                    //     this.FOLLOW.get(X).add(item);
-                                    // })
                                 }
-
-                                //  this.FOLLOW.get(X).push(...f);
                                 if (this.FOLLOW.has(X)) {
                                     f.forEach((item) => this.FOLLOW.get(X).add(item));
                                 } else {
@@ -178,12 +174,64 @@ class LL1 {
                     }
                 }
             }
-          console.log(this.FOLLOW,'KLK')
         }
     }
-
-
+    parseTable() {
+        for(let prod of this.G) {
+            for(let sel of prod.selection) {
+                let f = this.first(sel);
+                for(let terminal of f) {
+                    if(terminal === '@'){
+                        for(let term of this.FOLLOW.get(prod.noTerminal)){
+                           this.M.set(`${prod.noTerminal},${term}`,sel);
+                        }
+                    }else {
+                        this.M.set(`${prod.noTerminal},${terminal}`,sel);
+                    }
+                }
+            }
+        }
+    }
+    match(str) {
+        if(str.length === 1&& str === this.gra[this.gra.length -1]) {
+              return '';
+        }
+        if(this.gra.length === 0) {
+            this.gra.push([...this.VN][0]);
+        }
+        
+        let value;
+        for(let item of this.gra){
+            if(this.VN.has(item)){
+                let key = `${item},${str[0]}`;
+                value = this.M.get(key);
+                this.production = `${item} => ${value}`;
+                value = value.split('');
+                
+                for(let k in this.gra){
+                    if(this.gra[k] === item){
+                        this.gra.splice(k,1);
+                    }else{
+                        continue;
+                    }
+                   
+                    if(value[0] === '@'){ 
+                        return str;
+                    }
+                    this.gra.splice(k,0,...value);
+                    if(str[0] === value[0]) {
+                        str.shift();
+                        return str;
+                    }
+                    return str;
+                };
+                break;
+            }
+        }
+    }
 }
+
+
 
 let n = new LL1();
 let m = [
@@ -195,9 +243,11 @@ let m = [
 ]
 let g = m.map((item) => new Prod(item));
 g.forEach((item) => n.addProd(item));
-console.log(434, n.VN, n.VT)
+//console.log(434, n.VN, n.VT)
 let f = 'E->TG';
 m.forEach((item) => n.first(item));
 //n.first(f);
 n.follow();
-console.log(n.FOLLOW, 'follow');
+//console.log(n.FOLLOW, 'follow');
+n.parseTable();
+//console.log(n.M);
