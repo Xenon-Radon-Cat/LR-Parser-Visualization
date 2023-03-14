221d75fd9ab58fdc0a629526af14491658a4f8da
Xenon Radon Cat <w13815586587@163.com>
2023-03-14 17:45:22 +0800

    initial sketch for LR Parser Visualization

--- src/pages/LRgrammaAnalysis/index.js
+++ src/pages/LRgrammaAnalysis/index.js
@@ -1,6 +1,31 @@
+// top-level component of SLR Parser Visualization
 
-export function LRgramma() {
-    return <div>
-        LR语法分析
+import React from 'react';
+import { Col, Row} from 'antd';
+import { InputGrammar } from './InputGrammar';
+import './index.css'
+
+export const LRgramma = () => (
+    <div className='LRGrammar'>
+        <h1 className='header'> SLR Parser Visualization</h1>
+        <InputGrammar/>
+        {/*
+        <Row>
+            <Col span={12}>
+                Parse Table
+            </Col>
+            <Col span={12}>
+                Automation
+            </Col>
+        </Row>
+        <Row>
+            <Col span={12}>
+                Parse Expression
+            </Col>
+            <Col span={12}>
+                Parse Tree
+            </Col>
+        </Row>
+        */}
     </div>
-}
+)
