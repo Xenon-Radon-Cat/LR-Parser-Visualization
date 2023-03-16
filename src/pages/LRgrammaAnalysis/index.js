59b47f50d760185bbea2dac56b46ad18de082c0e
Xenon Radon Cat <w13815586587@163.com>
2023-03-16 21:27:08 +0800

    introduce Automation component

--- src/pages/LRgrammaAnalysis/index.js
+++ src/pages/LRgrammaAnalysis/index.js
@@ -1,35 +1,18 @@
 // top-level component of SLR Parser Visualization
 
-import React from 'react';
-import { Col, Row} from 'antd';
-import { InputGrammar } from './InputGrammar';
-import './index.css'
+import React from 'react'
 import { Provider } from 'react-redux'
 import store from './store'
+import './index.css'
+import { InputGrammar } from './InputGrammar'
+import { Automation } from './Automation'
 
 export const LRgramma = () => (
     <div className='LRGrammar'>
         <Provider store={store}>
         <h1 className='header'> SLR Parser Visualization</h1>
-        <InputGrammar/>
-        {/*
-        <Row>
-            <Col span={12}>
-                Parse Table
-            </Col>
-            <Col span={12}>
-                Automation
-            </Col>
-        </Row>
-        <Row>
-            <Col span={12}>
-                Parse Expression
-            </Col>
-            <Col span={12}>
-                Parse Tree
-            </Col>
-        </Row>
-        */}
+        <InputGrammar/>  
+        <Automation/>
         </Provider>
     </div>
 )
