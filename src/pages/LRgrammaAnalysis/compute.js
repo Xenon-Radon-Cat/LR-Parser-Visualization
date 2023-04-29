b8e10edf3151b79621efb24f59e76912f9f2c1c1
Xenon Radon Cat <w13815586587@163.com>
2023-04-29 15:33:53 +0800

    use set instead sorted array to search

--- src/pages/LRgrammaAnalysis/compute.js
+++ src/pages/LRgrammaAnalysis/compute.js
@@ -4,50 +4,63 @@ const computeAutomation = (grammar) => {
     // 'grammar': { productions: String[][], productionMap: Map<String, Number[]>, terminalSet: Set<String> }
     const { productions, productionMap } = grammar
 
-    const compareItem = (item1, item2) => {
-        // item: { productionIndex: Number, dotIndex: Number }
-        if(item1.productionIndex < item2.productionIndex)
-            return -1
-        else if(item1.productionIndex > item2.productionIndex)
-            return 1
-        else
-            return item1.dotIndex < item2.dotIndex ? -1 : 1
+    const encode = ({ productionIndex, dotIndex }) => {
+        // encode the binary group about item to number
+        return productionIndex * 65536 + dotIndex
+    }
+
+    const decode = (itemCode) => {
+        // decode the 'itemCode' to the binary group about item
+        return { productionIndex: Math.floor(itemCode / 65536), dotIndex: itemCode % 65536 }
     }
 
     const closure = (itemSet) => {
         // itemSet: [ productionIndex: Number, dotIndex: Number ]
-        // return sorted closure of 'itemSet'
+        // compute closure of 'itemSet' and generate the dot language description about automation when 'itemSet' has changed
 
+        // assert that each item in the 'itemSet' is unique
+        // 'seen' traces the items searched in the 'itemSet'
+        const seen = new Set()
+        for(const item of itemSet) {
+            seen.add(encode(item))
+        }
+        
         for(let i = 0; i < itemSet.length; ++i) {
             const { productionIndex, dotIndex } = itemSet[i]
             const token = productions[productionIndex][dotIndex]
+            let hasChanged = false // record whether the 'itemSet' has changed in the current iteration
 
             // check whether the 'token' following dot is a non-terminal
             if(productionMap.has(token)) {
                 // iterate all productions of 'token'
-                Loop:
                 for(let productionIndex of productionMap.get(token)) {
                     // check whether 'itemSet' contains the item { productionIndex, dotIndex: 2 }
-                    for(let i = 0; i < itemSet.length; ++i) {
-                        if(itemSet[i].productionIndex === productionIndex && itemSet[i].dotIndex === 2)
-                            continue Loop
+                    if(!seen.has(encode({ productionIndex, dotIndex: 2 }))) {
+                        // push new item { productionIndex, dotIndex: 2 } into 'itemSet'
+                        itemSet.push({ productionIndex, dotIndex: 2 })
+                        seen.add(encode({ productionIndex, dotIndex: 2 }))
+                        hasChanged = true
                     }
-                    // push new item { productionIndex, dotIndex: 2 } into 'itemSet'
-                    itemSet.push({ productionIndex, dotIndex: 2 })
                 }
             }
-        }
 
-        // sort and return
-        return itemSet.sort(compareItem)
+            // check whether the 'itemSet' has changed in the current iteration
+            if(hasChanged) {
+                // generate the dot language description about automation
+            }
+        }
     }    
 
-    const nodes = [closure([{ productionIndex: 0, dotIndex: 2}])]
+    const nodes = [[{ productionIndex: 0, dotIndex: 2 }]]
     const edges = [] // [{ sourceIndex: Number, targetIndex: Number, label: String }]
 
-    const computeAutomationHelper = (nodeIndex) => {
-        // assert that nodes['nodeIndex'] must be a closure
+    const computeAutomationHelper = (nodeIndex) => {     
         const edgeMap = new Map() // Map<String, [{ productionIndex: Number, dotIndex: Number }]>
+
+        // assert that all the itemSets in the 'nodes'[0, 'nodeIndex') are clousres
+        // assert that 'nodeIndex' is equal to 'nodes'.length - 1
+        closure(nodes[nodeIndex]) 
+        // assert that all the itemSets in the 'nodes' are closures
         
         // collect edges which start from 'nodeIndex'
         for(const { productionIndex, dotIndex } of nodes[nodeIndex]) {
@@ -63,34 +76,51 @@ const computeAutomation = (grammar) => {
 
         // dfs
         Loop1:
-        for(const [label, itemSet] of edgeMap.entries()) {
-            // compute closure of 'itemSet'
+        for(const [label, kernel] of edgeMap.entries()) {
+            // copy the 'kernel' and compute the closure of 'itemSet'
+            // we do not need to generate the dot language description about automation during closure('itemSet')
+            // since we only use the closure to judge whether we need to add a new node into automation
+            // if we have to add a new node into automation
+            // then we push 'kernel' into 'nodes' and compute the clousre of 'kernel' in the next recursion
+            // we would generate the dot language description about automation during closure('kernel')
+
+            const itemSet = kernel.map((item => ({ ...item })))
             closure(itemSet)
 
+            // assert that all the elements in the 'nodes' are closures
             // check whether 'nodes' contains 'itemSet'
             Loop2:
             for(const searchIndex in nodes) {
                 const searchItemSet = nodes[searchIndex]
-                // check whether the length of 'searchItemSet' is equal to 'itemSet's
+                // check whether the length of 'searchItemSet' is equal to 'itemSet'
                 if(searchItemSet.length !== itemSet.length)
                     continue
-                // iterate 'searchItemSet' to check the equality further
-                for(const itemIndex in searchItemSet) {
-                    const item = itemSet[itemIndex]
-                    const searchItem = searchItemSet[itemIndex]
-                    if(item.productionIndex !== searchItem.productionIndex || item.dotIndex !== searchItem.dotIndex)
+
+                // check whether 'searchItemSet' is equal to 'itemSet'
+                const seen = new Set() // 'seen' traces all the item in the 'searchItemSet'
+                for(const item of searchItemSet) {
+                    seen.add(encode(item))
+                }
+                for(const item of itemSet) {
+                    if(!seen.has(encode(item)))
                         continue Loop2
                 }
+
                 // success to search
                 edges.push({ sourceIndex: nodeIndex, targetIndex: Number(searchIndex), label})
                 continue Loop1
             }
 
             // fail to search
-            nodes.push(itemSet)
+            // assert that all the itemSets in the 'nodes' are closures
+            nodes.push(kernel)
             edges.push({ sourceIndex: nodeIndex, targetIndex: nodes.length - 1, label })
+            // assert that all the itemSets in the 'nodes'[0, 'nodes'.length - 1) are clousres
             computeAutomationHelper(nodes.length - 1)
+            // assert that all the itemSets in the 'nodes' are closures
         }
+
+        // assert that all the itemSets in the 'nodes' are closures 
     }
 
     computeAutomationHelper(0)
