46a1f2fd0a5c1e433f00575dada38ef0fb97c943
Xenon Radon Cat <w13815586587@163.com>
2023-04-29 20:40:29 +0800

    a component to render animated graphs by d3

---
+++ src/pages/LRgrammaAnalysis/Graphviz.js
@@ -0,0 +1,35 @@
+// a component which wraps 'd3' and 'd3-graphviz' to render graphs and animated transitions
+
+import { React, useEffect } from "react"
+import { graphviz } from "d3-graphviz"
+import * as d3 from "d3"
+
+export const Graphviz = (props) => {
+    // props: { identifier: string, dot: string }
+
+    const { identifier, dot } = props
+
+    const transitionFactory = () => {
+        return d3.transition()
+            .ease(d3.easeLinear)
+            .duration(500);
+    }
+
+    const options = {
+        height: 500, 
+        width: '100%', 
+        fit: true, 
+        zoomScaleExtent: [0.25, 4],
+    
+    }
+
+    useEffect(() => 
+    { 
+        graphviz(`#${identifier}`, options).transition(transitionFactory).renderDot(dot)
+    }, [identifier, dot])   
+
+    return (
+        <div id={identifier} className="Graphviz">
+        </div>
+    )
+}
